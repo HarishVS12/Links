4498cbd89c27bd7255c5a7298cf5c6aafe3442c6
HarishVS12 <harishv783@gmail.com>
2021-02-26 16:58:24 +0530

    Modify LinkPreview to get title and captions

--- app/src/main/java/com/linksofficial/links/utils/LinkPreview.kt
+++ app/src/main/java/com/linksofficial/links/utils/LinkPreview.kt
@@ -7,7 +7,7 @@ import org.jsoup.select.Elements
 import java.net.URI
 import java.net.URISyntaxException
 
-data class Properties(
+data class LinkProperties(
     var url: String? = null,
     var imageUrl: String? = null,
     var title: String? = null,
@@ -16,13 +16,46 @@ data class Properties(
 
 class LinkPreview(var url: String?) {
 
+    fun getLinkProperties():LinkProperties =
+        LinkProperties(
+            url = url,
+            imageUrl = getImageUrl(),
+            title = getTitle(),
+            description = getCaption()
+        )
+
+    private fun getElements(url: String): Elements {
+        return getDocument(url).getElementsByTag("meta")
+    }
+
+    private fun getDocument(url: String): Document {
+        return Jsoup.connect(url).timeout(30 * 1000).get()
+    }
+
+    private fun getCaption(): String {
+        var description = getElements(url ?: "").select("meta[name=description]").attr("content")
+        if (description.isEmpty() || description == null) {
+            description = getElements(url ?: "").select("meta[name=Description]").attr("content")
+        }
+        if (description.isEmpty() || description == null) {
+            description = getElements(url ?: "").select("meta[name=og:description]").attr("content")
+        }
+        if (description.isEmpty() || description == null) {
+            description = ""
+        }
+        return description
+    }
+
+    private fun getTitle(): String {
+        var title = getElements(url ?: "").select("meta[property=og:title]").attr("content")
+        if (title == null || title.isEmpty()) {
+            title = getDocument(url ?: "").title()
+        }
+        return title
+    }
 
     fun getImageUrl(): String {
-        var doc: Document? = null
-        doc = Jsoup.connect(url)
-            .timeout(30 * 1000)
-            .get()
-        val elements: Elements = doc.select("meta[property=og:image]")
+        val elements: Elements = getElements(url ?: "").select("meta[property=og:image]")
         if (elements.size > 0) {
             val image = elements.attr("content")
             if (!image.isEmpty()) {
